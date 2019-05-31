0064319e26727c8c92ee6667243037e25eff7d40
Stephan Rosenke <stephan.rosenke@ulb.tu-darmstadt.de>
2019-05-31 10:40:07 +0200

    Added some LC variables
    	modified:   blanko.bash

--- blanko.bash
+++ blanko.bash
@@ -23,9 +23,19 @@
 # set some non-user serviceable vars                                           #
 ################################################################################
 
-# unset LANG if you parse stdout-put of your commands for preventing problems
-# with unanticipated languages
+# unset LANG and LC variables if you parse stdout-put of your commands for
+# preventing problems with unanticipated languages
 #unset LANG
+#unset LC_ADDRESS
+#unset LC_IDENTIFICATION
+#unset LC_MEASUREMENT
+#unset LC_MESSAGES
+#unset LC_MONETARY
+#unset LC_NAME
+#unset LC_NUMERIC
+#unset LC_PAPER
+#unset LC_TELEPHONE
+#unset LC_TIME
 
 
 ################################################################################
